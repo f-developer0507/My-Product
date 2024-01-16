180782198667a4cf04fd3e102c4c0cc28654dbc1
f-developer0507 <stormpubg0507@gmail.com>
2024-01-16 19:00:55 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const darkMode = document.querySelector(".header__dark-mode");
+const body = document.querySelector("body");
+
+darkMode.addEventListener("click", function () {
+  body.classList.toggle("dark");
+});
+
+const burgerBtn = document.querySelector(".header__burger-btn");
+const clouseBtn = document.querySelector(".header__clouse-btn");
+const haederMobile = document.querySelector(".hader__nav");
+
+burgerBtn.addEventListener("click", function () {
+  haederMobile.classList.add("haeder__nav--mobile");
+});
+
+clouseBtn.addEventListener("click", function () {
+  haederMobile.classList.remove("haeder__nav--mobile");
+});
