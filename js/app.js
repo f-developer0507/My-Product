4a9226ba71b80f27469d3c66adb4e7a7ee9e9819
f-developer0507 <stormpubg0507@gmail.com>
2024-01-16 19:33:50 +0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
+// preloader js start
+const preloader = document.querySelector(".preloader-wrapper");
+window.onload = function () {
+  preloader.classList.add("preloader-hidden");
+};
+
+// preloader js end
+
+// dark mode js start
 const darkMode = document.querySelector(".header__dark-mode");
 const body = document.querySelector("body");
 
 darkMode.addEventListener("click", function () {
   body.classList.toggle("dark");
 });
+// dark mode js end
 
+// nav mobile js start
 const burgerBtn = document.querySelector(".header__burger-btn");
 const clouseBtn = document.querySelector(".header__clouse-btn");
 const haederMobile = document.querySelector(".hader__nav");
@@ -16,3 +27,4 @@ burgerBtn.addEventListener("click", function () {
 clouseBtn.addEventListener("click", function () {
   haederMobile.classList.remove("haeder__nav--mobile");
 });
+// nav mobile js end
